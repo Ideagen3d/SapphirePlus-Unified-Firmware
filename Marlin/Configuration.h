620038546c0c74f3162582c425ab37830ee607ad
Firedrops <31351436+Firedrops@users.noreply.github.com>
2022-05-02 17:47:56 +1000

    Update Configuration.h
    Always (re-)enable bed levelling (mesh) after auto-home/G28.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1556,7 +1556,7 @@
  * leveling immediately after G28.
  */
 //#define RESTORE_LEVELING_AFTER_G28
-//#define ENABLE_LEVELING_AFTER_G28
+#define ENABLE_LEVELING_AFTER_G28
 
 /**
  * Auto-leveling needs preheating
